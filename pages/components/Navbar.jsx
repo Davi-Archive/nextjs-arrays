af01a11a98c1d86ea92978c62d8f370cd240a1ff
davi38 <davi4alves@gmail.com>
2022-08-16 00:06:50 -0300

    # link ??

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props){
 return (
         <div className={styles.navItem}>
            <ul>
-           <Link href={props.destino} >
+           <Link href={props.destino ?? '#'} >
                  <li><a>{props.name}</a></li>
             </Link>
            </ul>
